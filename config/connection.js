c5881ababfff5685875e3f6efbc5be3d77550936
Alexander <agonz519@gmail.com>
2018-05-19 13:50:25 -0400

    Started setting up Heroku DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // Set up the MySQL connection...
 const mysql = require('mysql');
 // require('dotenv').config();
+let connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'p@ssw0rd',
-  database: 'burgers_db'
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'p@ssw0rd',
+    database: 'burgers_db'
+  });
+}
+
 
 // console.log(connection);
 
